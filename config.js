0605ced5d917ddbcfa180caf4011725790599ceb
lizartorres <cllizarazotorres@gmail.com>
2024-10-14 20:52:35 -0500

    https://github.com/lizartorres/Proyecto_Telematica.git

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const firebase = require("firebase");
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyByIps1wDChv-Car3QyvGHJapYQUnauLic",
-  authDomain: "telematicaiii.firebaseapp.com",
-  projectId: "telematicaiii",
-  storageBucket: "telematicaiii.appspot.com",
-  messagingSenderId: "934017795194",
-  appId: "1:934017795194:web:1be2025138580061f8e7c3"
+  apiKey: "AIzaSyCpaESU4TXDxYJ47L1ocTUa_HOwwG9-IRM",
+  authDomain: "proyectotelematica3-60bff.firebaseapp.com",
+  projectId: "proyectotelematica3-60bff",
+  storageBucket: "proyectotelematica3-60bff.appspot.com",
+  messagingSenderId: "605669491710",
+  appId: "1:605669491710:web:89db93b3667ed8075376af"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
