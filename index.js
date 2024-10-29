9e65669f0fb4bebc47b4d90120592c0863f0a5a3
lizartorres <cllizarazotorres@gmail.com>
2024-10-29 18:38:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -65,21 +65,40 @@ app.get("/Products/", async(req,res)=>{
 
   //AUTENTICACION
   
+// app.post("/Authentication/", async(req,res)=>{
+//     const snapshot= await User.get();
+//     const list = snapshot.docs.map((doc)=>({id:doc.id, ...doc.data()}))
+//     list.forEach(element => {
+//         if((element.User == req.body.User && element.Pass == req.body.Pass))
+//         {
+//             res.send("Valid")
+//             return false;
+//         }
+//         else
+//         {
+//             res.send("Not Valid")
+//             return true;
+//         }
+//     });
+//   });
+
+//AUTENTICACION
+  
 app.post("/Authentication/", async(req,res)=>{
     const snapshot= await User.get();
-    const list = snapshot.docs.map((doc)=>({id:doc.id, ...doc.data()}))
+    const list = snapshot.docs.map((doc)=>(doc.data()))
+    var result="No valido"
+    console.log(list)
     list.forEach(element => {
-        if((element.User == req.body.User && element.Pass == req.body.Pass))
-        {
-            res.send("Valid")
-            return false;
-        }
-        else
-        {
-            res.send("Not Valid")
-            return true;
-        }
+      if((element.User==req.body.User && element.Pass==req.body.Pass))
+      {
+        result="Usuario y contraseña valida";
+        return false;
+      }
+      console.log(element.User)
     });
-  });
+    res.send({msg:result})
+    });
 
   app.listen(4000,()=>console.log("Up & Running * 4000"))
+
